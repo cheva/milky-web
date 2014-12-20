b784bd378947b28023e359bb35b3191b752de822
Dmitry Cheva <dmitry.cheva@gmail.com>
2014-12-20 16:00:43 +0200

    $1

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app.blog.models import *
+from blog.models import *
 
 
 class PostAdmin(admin.ModelAdmin):
--- app/blog/models.py
+++ app/blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 from django.core.mail import send_mail
 
-from app.shared.utils import *
+from blog.shared.utils import *
 
 notify = False
 
