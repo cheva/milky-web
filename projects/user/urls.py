4fc7a815a1f6cf27fdd7517e8027b4cfa31efdb2
Dmitry Cheva <dmitry.cheva@gmail.com>
2014-12-29 03:28:29 +0200

    #2 user login: errors and messages

--- projects/user/urls.py
+++ projects/user/urls.py
@@ -4,4 +4,5 @@ from projects.user.views import *
 urlpatterns = patterns('',
                        (r'^$', index, {}, 'index'),
                        (r'^login/$', login, {}, 'login'),
+
 )
--- projects/user/views.py
+++ projects/user/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib import auth
 from helpers import functions
 
@@ -15,7 +16,25 @@ def index(request):
     return render(request, template, locals())
 
 
+def cabinet(request, pk):
+    """
+    Personal cabinet page
+    :param request:
+    :return render():
+    """
+    # @todo create cabinet page
+    messages.warning(request, '<h4>@todo create cabinet page!</h4>')
+    template = 'user/index.jinja'
+    local_vars = functions.get_local_vars(request)
+    return render(request, template, locals())
+
+
 def login(request):
+    """
+    Login controller
+    :param request:
+    :return HttpResponseRedirect();
+    """
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
@@ -23,12 +42,19 @@ def login(request):
         if user is not None and user.is_active:
             # User exists and active
             auth.login(request, user)
-            error_message = 'Ok!'
+            messages.success(request, '<h4>Successfully logged in!</h4>Congratulations!')
         else:
-            # Not active or not exists
-            error_message = 'Login error!'
+            # Not active or not exists or wrong password
+            if user is not None:
+                messages.error(request, '<h4>Login error!</h4>User is not active.')
+            else:
+                messages.error(request, '<h4>Login error!</h4>User is not registered or password error.')
     else:
         # Empty POST
-        error_message = "Wrong request!"
-    local_vars = functions.get_local_vars(request)
+        # @todo create static login page
+        messages.warning(request, '<h4>@todo create static login page!</h4>')
+        template = 'user/index.jinja'
+        local_vars = functions.get_local_vars(request)
+        return render(request, template, locals())
+
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
